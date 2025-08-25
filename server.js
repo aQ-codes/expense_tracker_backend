c0a95c5182ee1a1b49764ec788cb4a835f4aec10
Akhil Vijayakumar <73893083+aQ-codes@users.noreply.github.com>
2025-08-26 00:36:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "https://expense-tracker-user-frontend.vercel.app",
-  "https://expense-tracker-backend-j42j.onrender.com"
+  "https://expense-tracker-backend-j42j.onrender.com",
   process.env.FRONTEND_URL,
   process.env.FRONTEND_URL?.replace(/\/$/, ""),
 ].filter(Boolean);
